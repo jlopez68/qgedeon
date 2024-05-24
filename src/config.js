e21823e0127cedb28633ef2c5300833fdd7bb5ab
jlopez68 <jllp68@gmail.com>
2024-05-24 08:27:51 -0500

    3ero

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 2010;
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/qgedeon';
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/qgedeon';
